7abd0bdb1ecbd96d8119404cf119ae26f6213457
Arsham Muradyan <muradyanarsham311@gmail.com>
2024-07-01 12:11:34 +0400

    some changes in Link class

--- links.model.ts
+++ links.model.ts
@@ -1,49 +1,3 @@
-// class Link {
-//     static links = [];
-
-//     id: number;
-//     shortLink: string;
-//     longLink: string;
-
-//     constructor(id:number, shortLink:string) {
-//         this.id = id;
-//         this.shortLink = shortLink;
-//     }
-
-//     static getOne(linkId:number) {
-//         return Link.links.find(link => link.id === linkId);
-//     };
-
-//     static getAll() {
-//         return Link.links;
-//     };
-
-//     static createOne(shortLink:string, longLink:string) {
-//         const id = +new Date()
-//         const newLink = new Link(id, shortLink);
-//         Link.links.push(newLink);
-//         return newLink; 
-//     };
-
-//     static deleteOne(linkId:number) {
-//         const index = Link.links.findIndex(link => link.id === linkId);
-//         if (index !== -1) {
-//             return Link.links.splice(index, 1)[0];
-//         }
-//         return null;
-//     };
-
-//     static updateOne(linkId:number, {shortLink:string, longLink:string}) {
-//         const link = Link.getOne(linkId);
-//         if (link) {
-//             link.shortLink = shortLink;
-//             link.longLink = longLink;
-//             return link;
-//         }
-//         return null;
-//     };
-// }
-
 class Link {
     static links: Link[] = [];
 
@@ -99,10 +53,3 @@ class Link {
         return null;
     }
 }
-
-// Change js to ts/d
-// update one should accept 2 parametrs/d
-// create one shpuldn't accept id/d
-// getall should accept pagination/d
-// constructor should accept object without longlink/d
-// longlink should be getter (js getter and setter)/d
