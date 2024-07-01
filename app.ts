5fc8d19f2a3f9d36fb21bec0203d8d0b282e8ac0
Arsham Muradyan <muradyanarsham311@gmail.com>
2024-07-01 12:42:24 +0400

    adding tsconfig.json file

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req: any, res: { send: (arg0: string) => void; }) => {
     res.send("hi");
 });
 
