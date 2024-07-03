65734037094cef1d4bbae9e3ea99ebbf8962696a
Arsham Muradyan <muradyanarsham311@gmail.com>
2024-07-03 12:03:42 +0400

    initialization nodemon

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import linksRoutes from './routes/links';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use('/api', linksRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+});
---
+++ src/routes/links.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+import * as linksController from '../services/linksController';
+
+
+const router = Router();
+
+router.get('/links/:id', linksController.getOne);
+router.get('/links', linksController.getAll);
+router.post('links', linksController.createOne);
+router.delete('/links/:id', linksController.deleteOne);
+router.put('/links/:id', linksController.updateOne);
+
+export default router;
